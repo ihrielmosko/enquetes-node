c57b0f5b7ee8a6ae50de085841f42a5067278aec
ihriel mosko <ihriel.mosko@gmail.com>
2025-01-23 16:34:44 -0300

    mais funçoes database

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 import Joi from 'joi';
-import { readFile } from 'fs/promises';
 import express from 'express';
-import { getEnquetes, getEnquete, criarEnquete } from './database.js';
+import { getEnquetes, getEnquete, criarEnquete, altEnquete } from './database.js';
 import { create } from 'express-handlebars';
 const app = express();
 app.use(express.json());
@@ -15,7 +14,7 @@ app.set('view engine', 'handlebars');
 app.get('/', async (req, res) => {
     try {
         const enquetes = await getEnquetes();
-        res.render('home', {enquetes});
+        res.render('home', {enquetes, css: 'home.css'});
     }
     catch (err) {
         res.status(404).send(`página não localizada: ${err}`);
@@ -23,18 +22,11 @@ app.get('/', async (req, res) => {
 
 });
 
-app.get('/alterar', async (req, res) => {
+app.get('/visualizar/:id', async (req, res) => {
+    const { id } = req.params;
     try {
-        res.render('alterar');
-    }
-    catch (err) {
-        res.status(404).send(`página não localizada: ${err}`);
-    }
-});
-
-app.get('/visualizar', async (req, res) => {
-    try {
-        res.render('visualizar');
+        const enquete = await getEnquete(id)
+        res.render('visualizar', {enquete, css: 'visualizar.css'});
     }
     catch (err) {
         res.status(404).send(`página não localizada: ${err}`);
@@ -43,21 +35,43 @@ app.get('/visualizar', async (req, res) => {
 
 app.get('/criar', async (req, res) => {
     try {
-        res.render('criar');
+        res.render('criar', {css: 'criar.css'});
     }
     catch (err) {
         res.status(404).send(`página não localizada: ${err}`);
     }
 });
 
-app.post('/nova', async (req, res) => {
+app.get('/alterar/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        const enquete = await getEnquete(id)
+        enquete.data_inicio = new Date(enquete.data_inicio).toISOString().split('T')[0];
+        enquete.data_termino = new Date(enquete.data_termino).toISOString().split('T')[0];
+        res.render('alterar', {enquete, css: 'alterar.css'});
+    }
+    catch (err) {
+        res.status(404).send(`página não localizada: ${err}`);
+    }
+});
+
+app.post('/criar_enquete', async (req, res) => {
     try{
         const { titulo, dataIni, dataFim, op1, op2, op3, op4, op5, op6 } = req.body;
-        const enquete = await criarEnquete(titulo, dataIni, dataFim, op1, op2, op3, op4, op5, op6);
-        //res.status(201).send(enquete)
-        res.redirect('/');
+        await criarEnquete(titulo, dataIni, dataFim, op1, op2, op3, op4, op5, op6);
+        res.status(201).redirect('/');
     } catch (err) {
         res.status(404).send(`erro ao criar enquete: ${err}`);
+    }
+});
+
+app.post('/alterar_enquete', async (req, res) => {
+    try{
+        const { titulo, dataIni, dataFim, op1, op2, op3, op4, op5, op6 } = req.body;
+        await altEnquete(titulo, dataIni, dataFim, op1, op2, op3, op4, op5, op6);
+        res.status(201).redirect('/');
+    } catch (err) {
+        res.status(404).send(`erro ao alterar enquete: ${err}`);
     }
 });
 
